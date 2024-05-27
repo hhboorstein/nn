86ebaf239ce31b131b38b7b95aa87c441c77b68a
Harrison <107312633+hhboorstein@users.noreply.github.com>
2024-05-27 15:51:26 -0500

    tensor dimension transposition
    moved batch_size to be the first dimension of these tensors

--- transformer/PositionalEncoder.py
+++ transformer/PositionalEncoder.py
@@ -3,8 +3,9 @@ from torch import nn
 
 class PositionalEncoder(nn.Module):
 
-    def __init__(self, d_model: int, context_len = 4096, p_dropout: float = 0.05):
+    def __init__(self, d_model: int, context_len: int = 4096, p_dropout: float = 0.05):
         """
+
         Args:
           d_model: embedding dimension
           context_len: maximum length of sequence
@@ -19,10 +20,10 @@ class PositionalEncoder(nn.Module):
         base_change = torch.log(torch.tensor(10_000.0))
         denom       = torch.exp(base_change * dim_idx / d_model)
 
-        # encoding tensor, compatible with input of shape (seq_len, batch_size, emb_dim)
-        PE = torch.zeros(context_len, 1, d_model)  
-        PE[:, 0, 0::2] = torch.sin(pos_idx / denom)
-        PE[:, 0, 1::2] = torch.cos(pos_idx / denom)
+        # encoding tensor, compatible with input of shape (batch_size, context_len, emb_dim)
+        PE = torch.zeros(1, context_len, d_model)
+        PE[0, :, 0::2] = torch.sin(pos_idx / denom)
+        PE[0, :, 1::2] = torch.cos(pos_idx / denom)
 
         # declare module attributes
         self.register_buffer("PE", PE)
@@ -30,14 +31,12 @@ class PositionalEncoder(nn.Module):
 
     def forward(self, x):
         """
-
         Args:
-          x: Expects a tensor of shape (seq_len, batch_size, emb_dim)
-
-        Returns: encoded tensor of shape (seq_len, batch_size, emb_dim)
-
+          x: Expects a tensor of shape (batch_size, seq_len, emb_dim)
+        Returns: encoded tensor of shape (batch_size, seq_len, emb_dim)
         """
 
-        x = x + self.PE[:x.size(0)]
+        x = x + self.PE[:, :x.size(1), :]
         x = self.drop(x)
         return x
+        
